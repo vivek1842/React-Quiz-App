ff0ac303ba1b006505438f9a2a2a87ae887df657
vivek1842 <vivekghule2002@gmail.com>
2025-02-15 22:21:57 +0530

    que set added

--- src/data.js
+++ src/data.js
@@ -40,20 +40,44 @@ export const data = [
         ans: 3
     },
     {
-        "que": "What is the default value of the `mode` argument in `fs.open()`?",
-        "option1": "r",
-        "option2": "w",
-        "option3": "a",
-        "option4": "x",
-        "ans": 1
+        que: "What is the default value of the `mode` argument in `fs.open()`?",
+        option1: "r",
+        option2: "w",
+        option3: "a",
+        option4: "x",
+        ans: 1
     },
     {
-        "que": "Which of the following is NOT a method of the `fs` module?",
-        "option1": "fs.readFile()",
-        "option2": "fs.writeFile()",
-        "option3": "fs.createFile()",
-        "option4": "fs.appendFile()",
-        "ans": 3
+        que: "Which of the following is NOT a method of the `fs` module?",
+        option1: "fs.readFile()",
+        option2: "fs.writeFile()",
+        option3: "fs.createFile()",
+        option4: "fs.appendFile()",
+        ans: 3
+    },
+    {
+        que: "In which file system does the `fs` module operate in Node.js?",
+        option1: "HFS+",
+        option2: "NTFS",
+        option3: "EXT4",
+        option4: "Any supported file system",
+        ans: 4
+    },
+    {
+        que: "What does the `fs.readFileSync()` method do?",
+        option1: "Asynchronously reads a file",
+        option2: "Synchronously reads a file",
+        option3: "Writes data to a file",
+        option4: "Deletes a file",
+        ans: 2
+    },
+    {
+        que: "What does the `fs.existsSync()` method check?",
+        option1: "If a file is open",
+        option2: "If a file exists",
+        option3: "If a file can be written to",
+        option4: "If a file is empty",
+        ans: 2
     }
     
 ]
